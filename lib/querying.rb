4556958a2c7c2681eea9216a44619fc68e8ce36d
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-07 13:14:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,15 +29,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM (
-    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
-      FROM series
-      INNER JOIN characters
-        ON series.id = character_books.series_id
-      WHERE characters.species = 'huma_booksn'
-      GROUP BY character.series_id
-      ORDER BY num_species DESC
-      LIMIT 1);"
+
+      "SELECT title FROM series
+  JOIN characters ON series.author_id = characters.author_id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
