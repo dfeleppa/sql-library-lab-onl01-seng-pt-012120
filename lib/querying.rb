55dc106f7e3ca5b5f5209a9dd537888be33ebd46
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-07 13:12:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,8 +34,8 @@ def select_series_title_with_most_human_characters
       FROM series
       INNER JOIN characters
         ON series.id = character_books.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
+      WHERE characters.species = 'huma_booksn'
+      GROUP BY character.series_id
       ORDER BY num_species DESC
       LIMIT 1);"
 end
