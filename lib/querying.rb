9ee8cb481e6a7121ddfe064893288d9de15c6dd6
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-07 13:17:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,10 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) AS num_species FROM characters
-    GROUP BY species
-    ORDER BY num_species DESC
-    LIMIT 1;"
+  "SELECT species, count(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -42,14 +39,12 @@ end
 
 
 
-# def select_value_and_count_of_most_prolific_species
-#   "SELECT species, count(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
-# end
 
-# def select_name_and_series_subgenres_of_authors
-#   "SELECT authors.name, subgenres.name from series
-#   JOIN authors ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
-# end
+
+def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name from series
+  JOIN authors ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
+end
 
 # def select_series_title_with_most_human_characters
 #   "SELECT title FROM series
